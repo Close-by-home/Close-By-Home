d5f3546987338301cd548a128d3b7684ccb5dce4
Leandro de Sales Cotrim <56699403+leandroSalesCotrim@users.noreply.github.com>
2022-11-28 11:34:42 -0300

    Update FuncionarioController.java

--- src/main/java/closebyhome/closebyhome/controller/FuncionarioController.java
+++ src/main/java/closebyhome/closebyhome/controller/FuncionarioController.java
@@ -1,16 +1,14 @@
 package closebyhome.closebyhome.controller;
 
-import closebyhome.closebyhome.dto.CondominioDto;
 import closebyhome.closebyhome.dto.FuncionarioDto;
-import closebyhome.closebyhome.models.Funcionario;
+import closebyhome.closebyhome.dto.UsuarioDto;
 import closebyhome.closebyhome.service.FuncionarioService;
-
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+import java.util.List;
 
 @Tag(name = "Funcionario", description = "Requesição dos Funcionarios.")
 @RestController
@@ -18,30 +16,51 @@ import javax.validation.Valid;
 public class FuncionarioController {
     @Autowired
     private FuncionarioService funcionarioService;
-
     @GetMapping
-    public Boolean entrar(){
-
-        Boolean res = true ;
-
-        return res;
-    }
-
-    @PostMapping("{idUsario}")
-    public ResponseEntity<FuncionarioDto> cadastrarFuncionario(
-            @RequestBody @Valid FuncionarioDto novoFuncionario,
-            @PathVariable int idUsario
-    ){
-
-        FuncionarioDto res = funcionarioService.cadastrarFuncionario(novoFuncionario,idUsario);
-
-        if (res != null) {
-            return ResponseEntity.status(201).body(res);
-        } else {
-            return ResponseEntity.status(404).body(null);
+    public ResponseEntity<List<FuncionarioDto>> listar() {
+        List<FuncionarioDto> res = this.funcionarioService.buscarTodosFuncionarios();
+        if (res.isEmpty()) {
+            return ResponseEntity.status(204).build();
         }
+        return ResponseEntity.status(200).body(res);
     }
 
+    @GetMapping("{idCondominio}")
+    public ResponseEntity<List<FuncionarioDto>> listarPorCondominio(@PathVariable Integer idCondominio) {
+        List<FuncionarioDto> res = this.funcionarioService.buscarPorCondominio(idCondominio);
 
+        if (res.isEmpty()) {
+            return ResponseEntity.status(204).build();
+        }
+
+        return ResponseEntity.status(200).body(res);
+    }
+
+    @GetMapping("buscaPorNome/{nome}")
+    public ResponseEntity<List<FuncionarioDto>> listarPorNome(@PathVariable String nome) {
+        List<FuncionarioDto> res = this.funcionarioService.buscarFuncionarioPeloNome(nome);
+        if (res.isEmpty()) {
+            return ResponseEntity.status(204).build();
+        }
+        return ResponseEntity.status(200).body(res);
+    }
+
+    @GetMapping("buscaPorServico/{servico}")
+    public ResponseEntity<List<FuncionarioDto>> listarPorServico(@PathVariable String servico) {
+        List<FuncionarioDto> res = this.funcionarioService.buscarFuncionarioPeloNome(servico);
+        if (res.isEmpty()) {
+            return ResponseEntity.status(204).build();
+        }
+        return ResponseEntity.status(200).body(res);
+    }
+
+    @GetMapping("buscaPorNomeEServico/{nome}/{servico}")
+    public ResponseEntity<List<FuncionarioDto>> listarPorNomeEServico(@PathVariable String nome,@PathVariable String servico) {
+        List<FuncionarioDto> res = this.funcionarioService.buscarFuncionarioPeloServicoEnome(nome, servico);
+        if (res.isEmpty()) {
+            return ResponseEntity.status(204).build();
+        }
+        return ResponseEntity.status(200).body(res);
+    }
 
 }
